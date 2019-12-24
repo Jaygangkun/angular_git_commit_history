06adf77e6c09669abe842f2d5d598a5cf51f8853
JayGangkun <jaygangkun@hotmail.com>
2019-12-24 15:47:31 +0800

    show commits history

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GitApiService } from './gitapi.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'angulargitapi';
+  commits = null;
+
+  constructor(private gitApiService: GitApiService){
+    
+  }
+  ngOnInit(){
+    this.gitApiService.getCommitsHistory().subscribe((res) => {
+      this.commits = res;
+      console.log(this.commits);
+    })    
+  }
+
+  public convertDate(date){
+    return new Date(date);
+  }
 }
